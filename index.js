92b9f6379ce202df4afabd24202bd1128b0ad93d
xtripiansx <43681276+xtripiansx@users.noreply.github.com>
2020-07-15 19:54:41 +0200

    error sender_psid troubleshoot

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook', (req, res) => {
             console.log("webhook_event received");
 
             // Get the sender PSID
-            //let sender_psid = webhook_event.sender.id;
+            let sender_psid = webhook_event.sender.id;
             //console.log('Sender PSID: ' + sender_psid);
 
             // Check if the event is a message or postback and
